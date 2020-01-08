bb06494a95f46343a0a81bb146c8a7f5893e4fd6
AudreyCherrie <mitchelaudrey@gmail.com>
2020-01-08 17:56:18 +0300

    installed pillow inorder for the user to upload pictures in the application

--- ig/settings.py
+++ ig/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ig', 
-        'USER': 'Audrey',
-        'PASSWORD': 'MACHARIA',
+        'USER': 'macharia',
+        'PASSWORD': '0791731565',
     }
 }
 
--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .models import Profile,Image,Comment
 # Create your tests here.
+
+class ProfileTestClass(TestCase):
+
+    #set up method
+    def setUp(self):
+        self.audrey= Profile(first_name = 'Audrey', last_name ='Mish', email ='mitchelaudrey@gmail.com')
+
+    #testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.audrey,Profile))
