83fae9865da89568ec8f5e3cd0745c3316a47d67
AudreyCherrie <mitchelaudrey@gmail.com>
2020-01-09 09:54:14 +0300

    removed a bug on forms.py

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,7 +6,32 @@ from django.conf.urls.static import static
 
 urlpatterns=[
 url(r'^signup/',views.signup,name='signup'),
-url(r'^$',views.home,name='Home',),
+url(r'^$',views.home,from app import create_app,db
+from flask_script import Manager,Server
+from app.models import Writer,Subscribe,Post,Comments
+from flask_migrate import Migrate,MigrateCommand
+
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+migrate =Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+@manager.shell
+def make_shell_context():
+    return dict(app=app,db=db,Writer=Writer,Subscribe =Subscribe,Post=Post,Comments=Comments)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
+name='Home',),
 url(r'^timeline/',views.index,name='Index'),
 url(r'^profile/(\d+)',views.first_profile,name='Profile'),
 url(r'^images/',views.add_image,name='Image'),
